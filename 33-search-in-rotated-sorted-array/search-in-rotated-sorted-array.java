b37fc4367bc6bf43fd2b45b8ddfeb6575bb6b6e4
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-03-23 17:44:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (59.25%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -6,20 +6,19 @@ class Solution {
             int mid = low + (high - low) / 2;
 
             if (nums[mid] == target) return mid;
-
-            // Left half is sorted
             if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
+                if (nums[low] <= target && target <= nums[mid]) {
+                high = mid - 1;
+                }
+                else {
                     low = mid + 1;
                 }
             }
-            // Right half is sorted
             else {
-                if (nums[mid] < target && target <= nums[high]) {
+                if (nums[mid] <= target && target <= nums[high]){
                     low = mid + 1;
-                } else {
+                }
+                else {
                     high = mid - 1;
                 }
             }
