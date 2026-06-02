840d16ce030f6ed37301f83693362c92130da809
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-06-02 14:14:43 +0530

    Time: 27 ms (5.27%) | Memory: 42.2 MB (34.38%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -3,15 +3,20 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left = 1 , right = n , mid ;
-        while (left < right) {
-            mid = left + (right - left)/2;
-            if (isBadVersion(mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
+        //First Occurence Pattern
+        int lo = 1 ;
+        int hi = n;
+        int result = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            if (isBadVersion(mid)){
+                result = mid;
+                hi = mid - 1;
+            }
+            else {
+                lo = mid + 1;
             }
         }
-        return left;
+        return result;
     }
 }
