c530fcde8e958ee3d5d1f50ab9f7c69441416075
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-06-15 21:13:47 +0530

    Time: 109 ms (79.98%) | Memory: 46.9 MB (50.60%) - LeetSync

---
+++ 454-4sum-ii/4sum-ii.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        
+        for (int i : nums1) {
+            for (int j : nums2) {
+                int sum = i + j;
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        
+        for (int k : nums3) {
+            for (int l : nums4) {
+                count += map.getOrDefault(-(k + l), 0);
+            }
+        }
+        
+        return count;
+    }
+}
