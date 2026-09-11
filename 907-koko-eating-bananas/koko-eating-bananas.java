d2026befab0201e379763b6886c26d2cf0c74c1d
CrimsonLeaf <saminepallyjaswanth@gmail.com>
2026-09-11 07:21:48 +0530

    Time: 6 ms (99.76%) | Memory: 47.9 MB (42.57%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,34 +1,28 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        // The range for our binary search
-        int left = 1;
-        int right = 0;
-        for (int pile : piles) {
-            right = Math.max(right, pile);
+    public boolean canFinish(int[] piles,int h,int speed){
+        long totalhours=0;
+        for(int pile:piles){
+            totalhours+=(pile + speed - 1)/speed;
         }
-
-        int result = right;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (canFinish(piles, h, mid)) {
-                result = mid; // This speed works, but can we go slower?
-                right = mid - 1;
-            } else {
-                left = mid + 1; // Too slow, need to increase speed
+        return totalhours<=h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1;
+        int high=0;
+        for(int i=0;i<piles.length;i++){
+            high=Math.max(piles[i],high);
+        }
+        int result =high;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(canFinish(piles,h,mid)){
+                result=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-        
         return result;
-    }
-
-    private boolean canFinish(int[] piles, int h, int speed) {
-        long totalHours = 0; // Use long to prevent overflow
-        for (int pile : piles) {
-            // Equivalent to Math.ceil(pile / speed)
-            totalHours += (pile + speed - 1) / speed;
-        }
-        return totalHours <= h;
     }
 }
